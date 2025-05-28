eb178b803c0589f5205c06b06ec5ccaa818a83eb
Belobopka <belobaka1@gmail.com>
2025-05-28 21:57:41 +0300

    update prettier typo, update fetch/refetch

--- lynx/client/olx-parser/src/App.tsx
+++ lynx/client/olx-parser/src/App.tsx
@@ -1,60 +1,57 @@
 import React, { use, useRef } from 'react';
 
 import './App.css';
-import {
-  useQuery,
-} from '@tanstack/react-query'
-
-
+import { useQuery } from '@tanstack/react-query';
 
 const App = () => {
-  const { isPending, error, data } = useQuery({
+  const text = useRef('85000');
+  const { isPending, error, data, refetch } = useQuery({
     queryKey: ['repoData'],
     queryFn: () =>
-      fetch('http://127.0.0.1:3030/flats?price=85000').then((res) =>
+      fetch(`http://127.0.0.1:3030/flats?price=${text.current}`).then((res) =>
         res.json(),
       ),
-  })
-  const text = useRef()
-  if (isPending) return 'Loading...'
+  });
 
-  if (error) return 'An error has occurred: ' + error.message
+  if (isPending) return 'Loading...';
 
-  console.log(data)
+  if (error) return 'An error has occurred: ' + error.message;
 
   const renderElement = (element: any) => {
     return (
       <div>
         <div>
-        <a href={element.href}className="element_image"  target="_blank"><img src={element.image} className="element_image" /> </a>
+          <a href={element.href} className="element_image" target="_blank">
+            <img src={element.image} className="element_image" />
+          </a>
         </div>
         <div>
-        <span>{element.title}</span>
+          <span>{element.title}</span>
         </div>
         <div>
-        <span>{element.price}</span>
+          <span>{element.price}</span>
         </div>
         <div>
-        <span>{element.locationDate}</span>
+          <span>{element.locationDate}</span>
         </div>
       </div>
-    )
-  }
+    );
+  };
 
-const handleChangeText = (value: any) => {
-  text.current = value.nativeEvent.target.value
-}
-const handleClick = (value: any) => { fetch(`http://127.0.0.1:3030/flats?price=${text.current}`).then((res) =>
-        res.json(),
-      )
-}
+  const handleChangeText = (value: any) => {
+    text.current = value.nativeEvent.target.value;
+  };
+  const handleClick = (value: any) => {
+    refetch();
+  };
+
   return (
     <div>
-      <button onClick={handleClick}className="element_image"  />
-      <input onChange={handleChangeText}/>
+      <button onClick={handleClick} className="element_image" />
+      <input onChange={handleChangeText} />
       {data.map(renderElement)}
     </div>
-  )
+  );
 };
 
 export default App;
